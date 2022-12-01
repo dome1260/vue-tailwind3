49ab125e87b1b7531396e0d61ce8c4b015d15b44
Dome <dome@peerapongs-MacBook-Air.local>
2022-12-01 11:30:36 +0700

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <div class="bg-red-50">
+      Hello World
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView'
+}
+</script>
